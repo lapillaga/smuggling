416c8f46dc4fe962a18bb4bec626141af3b20401
Luis Pillaga <luispillaga@Luiss-MacBook-Pro.local>
2023-09-06 21:44:39 -0500

    Frist commit

---
+++ src/main/java/com/jhojytech/ec/smuggling/SmugglingApplication.java
@@ -0,0 +1,13 @@
+package com.jhojytech.ec.smuggling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmugglingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmugglingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhojytech/ec/smuggling/SmugglingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhojytech.ec.smuggling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmugglingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
